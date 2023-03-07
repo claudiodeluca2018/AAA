51767d88451c7d641520ab87e87da42c5f00a3ae
claudiodeluca2018 <claudiodeluca2018@gmail.com>
2023-03-07 15:45:23 -0300

    segundo comentario impactado para validar si la regla esta en funcionamiento

--- dos.cs
+++ dos.cs
@@ -1,3 +1,6 @@
 dos
 
 //comentario incluido para testear si la regla me impide hacer un push
+
+//Segundo comentario impactado debido a que el primero no ejecuto por que no lo habia asignado a ninguna
+//rama
